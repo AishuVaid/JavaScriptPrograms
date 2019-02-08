13e16d20d7ee3b08a22bbe81ef6ef820a3d43462
AishuVaid <aishu.vaid@gmail.com>
2019-02-08 19:24:06 +0530

    object oriented

--- Oops!/inventory.js
+++ Oops!/inventory.js
@@ -1,38 +1,57 @@
-const util=require('../Utility/OopsUtil');
-const fileStream=require('fs');
-const json1=fileStream.readFileSync('rice.json')
-const json2=fileStream.readFileSync('rice.json')
-const json3=fileStream.readFileSync('rice.json')
+var fileStream=require('fs');
+var UtilityOops=require('../Utility/OopsUtil');
+var input=fileStream.readFileSync('inventory.json');
+var object=JSON.parse(input);
+//const json1=fileStream.readFileSync('rice.json')
+//const json2=fileStream.readFileSync('rice.json')
+//const json3=fileStream.readFileSync('rice.json')
 //const access=fileStream.readFileSync('inventory.json');
-var object=JSON.parse(json);
-var inv1=new util.Rice(name,weight,price)
-var inv2=new util.Pulses(name,weight,price)
-var inv3=new util.Wheat(name,weight,price)
-for(let key1 in object1.Rice)
+
+var sum=0;
+var d=object.Rice;
+var e=object.Wheat;
+var f=object.Pulses;
+
+//var inv1=new util.Rice(name,weight,price)
+//var inv2=new util.Pulses(name,weight,price)
+//var inv3=new util.Wheat(name,weight,price)
+for(let i in d)
 {
-    var name=object1.Rice[key].name;
-    var weight=object1.Rice[key].weight;
-    var price=object1.Rice[key].price;
-    console.log("The total value of "+object1.Rice[key1].name+" is "+inv1.addValue(weight,price))
+    var name=d[i].name;
+    var weight=d[i].weight;
+    var rate=d[i].rate;
+    var total=d[i].weight*d[i].rate
+    console.log("The total value of ",d[i].name,"is",total);
+    sum=sum+total;
+    i=new UtilityOops.Rice(name,weight,rate)
 }
-for(let key2 in object2.Pulses)
+console.log(sum);
+console.log();
+
+
+for(let j in e)
 {
-    var name=object2.Pulses[key].name;
-    var weight=object2.Pulses[key].weight;
-    var price=object2.Pulses[key].price;
-    console.log("The total value of "+object2.Pulses[key2].name+" is "+inv2.addValue(weight,price)) 
+    var name=e[j].name;
+    var weight=e[j].weight;
+    var rate=e[j].rate;
+    var total=e[j].weight*e[j].rate;
+    console.log("The total value of "+e[j].name+" is ",total);
+    sum=sum+total;
+    j=new UtilityOops.Wheat(name,weight,rate)
 }
-for(let key3 in object3.Wheat)
+console.log(sum);
+console.log();
+
+
+for(let k in f)
 {
-    var name=object3.Wheat[key].name;
-    var weight=object3.Wheat[key].weight;
-    var price=object3.Wheat[key].price;
-    console.log("The total value of "+object3.Wheat[key3].name+" is "+inv1.addValue
-    
-    (weight,price))
+    var name=f[k].name;
+    var weight=f[k].weight;
+    var rate=f[k].rate;
+    var total=f[k].weight*f[k].rate;
+    console.log("The total value of "+f[k].name+" is "+total);
+    sum=sum+total;
+    k=new UtilityOops.Pulses(name,weight,rate)
 }
-function inventory()
-{
-    utilOops.inventory(object);
-}
-inventory();
+console.log(sum);
+console.log();
--- Utility/OopsUtil.js
+++ Utility/OopsUtil.js
@@ -1,5 +1,5 @@
-//var readline=require('readline-sync');
-//var fileStream=require('fs');
+var read=require('readline-sync');
+var file=require('fs');
 
 
 class Stock
@@ -61,45 +61,90 @@ class Rice
     }
 }
 
-
-
-
-
-module.exports={Stock,inventory,
-
-
-
-
-    inventory(object)
+class inventoryManage
+{
+    constructor()
     {
-        var rice=object.rice;
-        var pulses=object.pulses;
-        var wheat=object.wheat;
 
-        for(let key in rice)
+    }
+add(data)
+{
+    var name=read.question("Enter the name of the stock to add :")
+    var shares=read.questionInt("Enter the number of Shares :")
+    var quantity=read.questionInt("Enter the price of the shares :")
+    data.Stock.push(
         {
-            console.log("The total rate of "+rice[key].name+" for "+rice[key].weight+"kg = "+rice[key].weight*rice[key].rate)
-            
-        }
-        for(let key in pulses)
-        {
-            console.log("The total rate of "+pulses[key].name+" for "+pulses[key].weight+"kg ="+pulses[key].weight*pulses[key].rate)
-            
-        }
-        for(let key in wheat)
-        {
-            console.log("The total rate of "+wheat[key].name+" for "+wheat[key].weight+"kg ="+wheat[key].weight*wheat[key].rate)
-            
-        }
-    },
+            shareName : name,
+            noOfShares : shares,
+            price : quantity
+        })
+        var d=file.writeFileSync('inventoryManage.json',JSON.stringify(data))
 
-    replaceUsingRegex(name,fullName,num,day,fileString)
+}
+remove(data)
+{
+    var name=read.question("Enter the name of the stock you want to remove :")
+    for(let i=0;i<data.Stock.length;i++)
     {
-       
+        if(data.Stock[i].shareName==name)
+        {
+            var index=data.Stock.indexOf(data.Stock[i])
+            data.Stock.splice(index,1)
+        }
+        var d=file.writeFileSync('inventoryManage.json',JSON.stringify(data))
+    }
+}
 
-        fileString=fileString.replace(/<<name>/g,name)
-        fileString=fileString.replace(/<<fullName>>/g,fullName)
-        fileString=fileString.replace(/91-XXXXXXXXXXX/g,num)
+print(data)
+{
+    var stock=data.Stock;
+    for(const key in stock)
+    {
+        console.log(stock[key]);
+    }
+}
+
+edit(data)
+{
+    var stock=data.Stock;
+    var name=read.question("Enter the stock name to edit :")
+    var sharess=read.question("Enter no of shares to edit :")
+    var money=read.question("Enter the shares price :")
+    for(let key in stock)
+    {
+        if(stock[key].shareName==name)
+        var value=key;
+        if(stock[key].noOfShares==sharess)
+        var value1=key;
+        if(stock[key].price==money)
+        var value2=key;
+    }
+    var name1=read.question("Enter the new name of the share :");
+    stock[value].shareName=name1;
+    var name2=read.question("Enter the new no of shares");
+    stock[value1].noOfShares=name2;
+    var name3=read.question("Enter the new price of the shares :")
+    stock[value2].price=name3;
+    var d=file.writeFileSync('inventoryManage.json',JSON.stringify(data))
+}
+
+}
+module.exports={Stock,Rice,Wheat,Pulses,inventoryManage,
+
+
+
+ 
+
+    replaceUsingRegex(name,fullName,num,day)
+    {
+       var fs = require('fs');
+       var fileString = fs.readFileSync('oops.txt','utf8');
+
+        fileString=fileString.replace(/<<name>>/g,name)
+        fileString=fileString.replace(/<<full name>>/g,fullName)
+        fileString=fileString.replace(/<<xxxxxxxxxx>>/g,num)
+        var date = new Date();
+        day = date.toDateString();
         fileString=fileString.replace(/01-01-2016/g,day);
         return fileString;
     },
